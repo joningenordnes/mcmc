c4ec8827e54ab16bd11b93c1f4922af3a04a30c6
Jon Inge Nordnes <joninge@allegro.no>
2023-10-06 12:42:55 +0200

    Nojekyl

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     output: 'export',
-    distDir: 'docs',
-    assetPrefix: 'https://www.mcmc.no/',
+    distDir: 'docs'
 }
 
 module.exports = nextConfig
