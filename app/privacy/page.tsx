cc0030a55476a9cc8c46875c8723930224653b9f
Jon Inge Nordnes <joninge@allegro.no>
2024-02-15 10:46:57 +0100

    Added privacy.

---
+++ app/privacy/page.tsx
@@ -0,0 +1,37 @@
+import "./simple.min.css"
+
+const Page = () => {
+    return (
+        <div>
+            
+            <h1>Privacy Policy</h1>
+            <p>This privacy policy governs your use of the MCMC mobile application.</p>
+
+            <h2>Information Collection and Use</h2>
+            <p>The MCMC does not collect any personal information or data.</p>
+
+            <h2>Log Data</h2>
+            <p>No log data is collected when you use the MCMC mobile application.</p>
+
+            <h2>Cookies</h2>
+            <p>MCMC does not use cookies.</p>
+
+            <h2>Security</h2>
+            <p>The security of your information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+
+            <h2>Links to Other Sites</h2>
+            <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+
+            <h2>Children's Privacy</h2>
+            <p>This Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to take the necessary actions.</p>
+
+            <h2>Changes to This Privacy Policy</h2>
+            <p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.</p>
+
+            <h2>Contact Us</h2>
+            <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at sendron@gmail.com.</p>
+
+        </div>
+    );
+};
+export default Page;
---
+++ docs/_next/static/chunks/app/privacy/page-f0a66c9a1afdac29.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[385],{2546:function(n,e,u){Promise.resolve().then(u.t.bind(u,2234,23))},2234:function(){}},function(n){n.O(0,[971,864,744],function(){return n(n.s=2546)}),_N_E=n.O()}]);
