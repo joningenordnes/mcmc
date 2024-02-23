4b18c5f9dedb5d312379dac89a74d02adf61b695
Jon Inge Nordnes <joninge@allegro.no>
2024-02-23 09:00:19 +0100

    Link to app.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import React from "react"
 import Logo from "@/components/Logo"
 
 const Page = () => {
     return (
-        
+        <div className="mCC">
             <Logo/>
-        
+            <a href="https://play.google.com/store/apps/details?id=com.jinordnes.MCMC" target="_blank">
+                <img src="/android-market-icon.png" alt="image" width={200} height="auto"/>
+            </a>
+        </div>
     )
 }
 export default Page
